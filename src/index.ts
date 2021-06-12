7a116f312b3786745d9c698b767234032700103b
Felix Häberle <34959078+felixhaeberle@users.noreply.github.com>
2021-06-12 09:44:32 +0000

    update export of cascadeDelete

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,7 @@
 const functions = require("./functions");
 
-const cascadeDelete = async (prisma, modelName: String, where) => {
+export const cascadeDelete = async (prisma, modelName: String, where) => {
     const info =await functions.getDeletionInfo(prisma, modelName,where);
     console.log(info)
     await prisma.$transaction(info.map(inf=>functions.createDeletions(prisma,inf)))
 };
-
-module.exports = { cascadeDelete };
